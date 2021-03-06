4ddfc88df6e83e29d36b1fd570b2634d3e6f5176
yoshida <hcaeerr.221@gmail.com>
2021-03-06 04:06:44 +0000

    phplaravel16課題

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Profile;
 
+use App\History;
+
+use Carbon\Carbon;
+
 class ProfileController extends Controller
 {
     //
@@ -15,28 +19,80 @@ class ProfileController extends Controller
     }
     public function create(Request $request)
     {
-        // Varidationを行う
         $this->validate($request, Profile::$rules);
-        
         $profile = new Profile;
         $form = $request->all();
-        
+      
         // フォームから送信されてきた_tokenを削除する
         unset($form['_token']);
         // フォームから送信されてきたimageを削除する
         unset($form['image']);
-        
+
         // データベースに保存する
         $profile->fill($form);
-        $profile->save();    
+        $profile->save();
+        
+        // admin/news/createにリダイレクトする
         return redirect('admin/profile/create');
     }
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
+    
+    public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          // 検索されたら検索結果を取得する
+          $posts = Profile::where('title', $cond_title)->get();
+      } else {
+          // それ以外はすべてのニュースを取得する
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+  public function edit(Request $request)
+  {
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
+      return view('admin.profile.edit', ['profile_form' => $profile]);
+  }
+  
+  public function update(Request $request)
+  {
+      // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      if (isset($profile_form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $profile->image_path = basename($path);
+        unset($profile_form['image']);
+      } elseif (isset($request->remove)) {
+        $profile->image_path = null;
+        unset($profile_form['remove']);
+      }
+      unset($profile_form['_token']);
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+      
+      $history = new History;
+        $history->news_id = $profiles->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
+
+      return redirect('admin/profile');
+  }
+  
+// 以下を追記　　
+  public function delete(Request $request)
+  {
+      // 該当するNews Modelを取得
+      $profile = Profile::find($request->id);
+      // 削除する
+      $profile->delete();
+      return redirect('admin/profile/');
+  } 
 }
